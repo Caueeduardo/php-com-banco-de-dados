e1daa80c1bd3348da4e11c1c9c5621ae186fef81
Gelvazio <gelvazio@hotmail.com>
2024-09-18 22:01:35 -0300

    OK

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
 // require_once("conexao/index.php");
-require_once("ConsultaSistema.php");
+// require_once("ConsultaSistema.php");
+// require_once("ConsultaUsuario.php");
+
+// http://localhost/php-com-banco-de-dados/sistema-escolar-sem-bd/
+echo '<a href="ConsultaSistema.php">Consulta Sistema</a>';
+echo '</br>';
+echo '<a href="ConsultaUsuario.php">Consulta Usuario</a>';
+echo '</br>';
+echo '<a href="sistema-escolar-sem-bd/index.php">Sistema Escolar Sem Banco Dados</a>';
